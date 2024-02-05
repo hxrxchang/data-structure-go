3590df1147bd1738d050c6b847b7efbb8544bea0
hxrxchang <mail@hxrxchang.dev>
2024-02-06 01:37:39 +0900

    implement linked list

--- list/list.go
+++ list/list.go
@@ -5,8 +5,55 @@ type Element[T any] struct {
 	Next *Element[T]
 }
 
-func NewElement[T any](value T, next *Element[T]) *Element[T] {
-	return &Element[T]{Value: value, Next: next}
+
+type List[T any] struct {
+	First *Element[T]
 }
 
+func NewList[T any]() *List[T] {
+	return &List[T]{}
+}
 
+func (l *List[T]) Unshift(value T) {
+	l.First =  &Element[T]{
+		Value: value,
+		Next: l.First,
+	}
+}
+
+func (l *List[T]) Push(value T) {
+	if l.First == nil {
+		l.First = &Element[T]{
+			Value: value,
+			Next: nil,
+		}
+	} else {
+		var last *Element[T]
+		for e := l.First; e != nil; e = e.Next {
+			last = e
+		}
+		last.Next = &Element[T]{
+			Value: value,
+			Next: nil,
+		}
+	}
+}
+
+func (l *List[T]) Each() []*Element[T] {
+	var result []*Element[T]
+	for e := l.First; e != nil; e = e.Next {
+		result = append(result, e)
+	}
+	return result
+}
+
+func (l *List[T]) Find(idx int) *Element[T] {
+	i := 0
+	for e := l.First; e != nil; e = e.Next {
+		if i == idx {
+			return e
+		}
+		i++
+	}
+	return nil
+}
--- list/list_test.go
+++ list/list_test.go
@@ -1,21 +1,63 @@
 package list
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestNewElement(t *testing.T) {
-	ele1 := NewElement[int](1, nil)
-	if ele1.Value != 1 {
-		t.Errorf("ele.Value = %v; want 1", ele1.Value)
-	}
-	if ele1.Next != nil {
-		t.Errorf("ele.Next = %v; want nil", ele1.Next)
+func TestUnshift(t *testing.T) {
+	l := NewList[int]()
+	l.Unshift(4)
+	l.Unshift(3)
+	l.Unshift(1)
+	l.Unshift(2)
+
+	expected := []int{2, 1, 3, 4}
+	got := l.Each()
+
+	if len(got) != len(expected) {
+		t.Fatalf("expected %v, got %v", expected, got)
 	}
 
-	ele2 := NewElement[int](2, ele1)
-	if ele2.Value != 2 {
-		t.Errorf("ele.Value = %v; want 2", ele2.Value)
+	for i := 0; i < len(expected); i++ {
+		if got[i].Value != expected[i] {
+			t.Fatalf("expected %v, got %v", expected[i], got[i].Value)
+		}
 	}
-	if ele2.Next != ele1 {
-		t.Errorf("ele.Next = %v; want %v", ele2.Next, ele1)
+}
+
+func TestPush(t *testing.T) {
+	l := NewList[int]()
+	l.Push(4)
+	l.Push(3)
+	l.Push(1)
+	l.Push(2)
+
+	expected := []int{4, 3, 1, 2}
+	got := l.Each()
+
+	if len(got) != len(expected) {
+		t.Fatalf("expected %v, got %v", expected, got)
+	}
+
+	for i := 0; i < len(expected); i++ {
+		if got[i].Value != expected[i] {
+			t.Fatalf("expected %v, got %v", expected[i], got[i].Value)
+		}
+	}
+}
+
+func TestFind(t *testing.T) {
+	l := NewList[int]()
+	l.Push(4)
+	l.Push(3)
+	l.Push(1)
+	l.Push(2)
+
+	expected := []int{4, 3, 1, 2}
+	for i := 0; i < 4; i++ {
+		got := *l.Find(i)
+		if got.Value != expected[i] {
+			t.Fatalf("expected %v, got %v", expected[i], got.Value)
+		}
 	}
 }
